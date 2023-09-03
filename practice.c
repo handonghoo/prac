824bb16848869f1886487c012ce6926907bca87b
handonghoo <handonghoo0812@gmail.com>
2023-09-03 16:14:14 +0900

    first commit

---
+++ practice.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int num;
+
+    printf("썩을 c언어: ");
+    scanf("%d", &num);
+
+    printf("입력한 값: %d\n", num);
+
+    return 0;
+}
+
+v
---
+++ practice.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World! 안농" << std::endl;
+    return 0;
+}
